9817e7bc8fc34b2012b112c9b449708bd21f8ac6
unknown <Academico@D08KPC24A303.instructores.senati.local>
2023-08-14 17:10:51 -0500

    router-dom

--- src/Asistente.jsx
+++ src/Asistente.jsx
@@ -1,29 +1,57 @@
 import React, { useEffect } from 'react';
 
-const Asistente = () => {
+ 
+
+function Asistente() {
+
   useEffect(() => {
+
     window.watsonAssistantChatOptions = {
-      integrationID: "84c273b5-3bc9-4724-bff7-21ad4a784485",
+
+      integrationID: "3848d12b-27cb-4657-8522-f1b7aa24842d",
+
       region: "au-syd",
-      serviceInstanceID: "e322a0b7-49b4-4ba9-9f62-f88d3c94e5aa",
+
+      serviceInstanceID: "6f966fc9-7eb1-4277-8480-63099f45f540",
+
       onLoad: function(instance) { instance.render(); }
+
     };
+
+ 
 
     const script = document.createElement('script');
+
     script.src = "https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+
     document.head.appendChild(script);
 
+ 
+
     return () => {
-      // Cleanup: remove the script when the component unmounts
+
+      // Cleanup: Remove the script when the component is unmounted
+
       document.head.removeChild(script);
+
     };
-  }, []); // Empty dependency array ensures the effect runs only once
+
+  }, []);
+
+ 
 
   return (
+
     <div>
-      {/* You can add any additional content or components here */}
+
+      {/* You can add any other components or content here */}
+
     </div>
+
   );
-};
+
+}
+
+ 
 
 export default Asistente;

29064c04c6df10bad545b2da54854bb279806411
unknown <Academico@D08KPC24A303.instructores.senati.local>
2023-08-14 18:55:25 -0500

    avance2

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -13,11 +13,11 @@ const Nav = () => {
         
         }}>
             
-            <div className='flex justify-content-between align-items-center gap-2 w-11 p-3'>
+            <div className='flex justify-content-end align-items-center gap-2 w-11 p-3'>
                 <p>Bonitos y Gorditos</p>
                 <InputText className='flex-1 h-2rem border-round-2xl'/>
-                <Button className='bg-black-alpha-90 border-black-alpha-90 ' label='Iniciar Sesion'/>
-                <Button className='bg-white-alpha-90 border-white-alpha-90  text-black-alpha-90  font-weight-bold' label='Cerrar Sesion'/>
+                <Button className=' bg-black-alpha-90 border-black-alpha-90 ' label='Iniciar Sesion'/>
+                <Button className=' bg-white-alpha-90 border-white-alpha-90  text-black-alpha-90  font-weight-bold' label='Cerrar Sesion'/>
             </div>
         </div>
     )
